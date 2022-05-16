02e0299649134fe0558c9a0dacd396ae0b3e7f1a
Kleptoman13 <asdfghjkl38939@gmail.com>
2022-05-16 11:21:50 +0300

    AddEveryting

--- src/com/company/Drob.java
+++ src/com/company/Drob.java
@@ -4,6 +4,9 @@ public class Drob {
     private int a;
     private int b;
 
+    private double delAB;
+
+
     public Drob(int a, int b)
     {
         this.setA(a);
@@ -26,5 +29,35 @@ public class Drob {
         return b;
     }
 
+    public double findDrob()
+    {
+        delAB = (double)getA()/getB();
+        return delAB;
+    }
 
+
+    public double ChangedDrob()
+    {
+        double temp1 = getA();
+        double temp2 = getB();
+        return temp2 / temp1;
+    }
+
+    public double StepenDrob(int stepen)
+    {
+        int temp1 = (int)Math.pow(getA(), stepen);
+        int temp2 = (int)Math.pow(getB(), stepen);
+        return temp1/temp2;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Drob{" +
+                "a/b=" + getA() +
+                "/" + getB() +
+                ", " + findDrob() +
+                ", b/a=" + ChangedDrob() +
+                "}";
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        final int NumberDrobs = 3;
+        Random random = new Random();
+        Drob[] drob1 =new Drob[NumberDrobs];
+        Drobs drobs = new Drobs(NumberDrobs);
+        for (int i = 0; i < NumberDrobs; i++) {
+            drob1[i] = new Drob(random.nextInt(10) + 1, random.nextInt(10) + 1);
+            drobs.addDrob(drob1[i]);
+        }
 
+        System.out.println(drobs);
+
+        System.out.println("d1 = " + drobs.findD1());
+        System.out.println("d2 = " + drobs.findD2());
     }
+
 }
