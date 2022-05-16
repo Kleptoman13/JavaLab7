cfc5c9222c26c6015120ffd74377d86d2fbedbd5
Kleptoman13 <asdfghjkl38939@gmail.com>
2022-05-16 10:08:43 +0300

    Initial commit

---
+++ src/com/company/Drob.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Drob {
+    private int a;
+    private int b;
+
+    public Drob(int a, int b)
+    {
+        this.setA(a);
+        this.setB(b);
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+
+}
